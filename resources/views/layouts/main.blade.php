e193fa4b5fadeb73a9d3e8262a41fe41553d0354
Satrio <satriotol69@gmail.com>
2021-01-11 17:53:56 +0700

    pwa

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -15,9 +15,10 @@
         crossorigin="anonymous" />
     <link rel="shortcut icon" href="{{asset('favicon.ico')}}" type="image/x-icon">
     <link rel="icon" href="{{asset('favicon.ico')}}" type="image/x-icon">
-    <link rel="manifest" href="{{asset('landingpage/manifest.json')}}">
+    {{-- <link rel="manifest" href="{{asset('landingpage/manifest.json')}}"> --}}
 
     <title>BEAUTY AND CARE</title>
+    @laravelPWA
 </head>
 
 <body>
@@ -46,6 +47,36 @@
         $(document).ready(function () {
             $('.image-link').magnificPopup({
                 type: 'image'
+            });
+        });
+
+    </script>
+    <script>
+        let deferredPrompt;
+        const addBtn = document.querySelector('.add-button');
+        addBtn.style.display = 'none';
+        window.addEventListener('beforeinstallprompt', (e) => {
+            // Prevent Chrome 67 and earlier from automatically showing the prompt
+            e.preventDefault();
+            // Stash the event so it can be triggered later.
+            deferredPrompt = e;
+            // Update UI to notify the user they can add to home screen
+            addBtn.style.display = 'block';
+
+            addBtn.addEventListener('click', (e) => {
+                // hide our user interface that shows our A2HS button
+                addBtn.style.display = 'none';
+                // Show the prompt
+                deferredPrompt.prompt();
+                // Wait for the user to respond to the prompt
+                deferredPrompt.userChoice.then((choiceResult) => {
+                    if (choiceResult.outcome === 'accepted') {
+                        console.log('User accepted the A2HS prompt');
+                    } else {
+                        console.log('User dismissed the A2HS prompt');
+                    }
+                    deferredPrompt = null;
+                });
             });
         });
 
